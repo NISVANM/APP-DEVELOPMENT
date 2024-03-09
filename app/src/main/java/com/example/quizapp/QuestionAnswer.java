f633471eac0a50a5eee456ad9941a3c7c71551ca
NISVAN--MOHAMED <niswan19@gmail.com>
2024-03-09 21:33:10 +0530

    Finished project

---
+++ app/src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,20 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+
+    public static String question[]={
+            "who invented tv?",
+            "who is the author of alchemist?",
+            "who is the founder of spacex?"
+    };
+    public static String choices [][]={
+            {"john lodge" ,"graham bell","issac newton"},
+            {"paulo coele","abrahm","roy smith"},
+            {"bill gates","elon musk","roy thomas"}
+};
+    public static String correctanswers []={
+            "john lodge",
+            "paulo coele",
+            "elon musk"
+    };
+}
